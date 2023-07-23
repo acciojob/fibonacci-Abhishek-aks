df500ed4a07ddb4081be394f20f8cb7b8aa2f885
Abhishek-aks <117778085+Abhishek-aks@users.noreply.github.com>
2023-07-24 04:46:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function fibonacci(num) {
 // your code here
 	let prev = 0, curr = 1;
 	// if( num == 0) return 0;
-    if( num == 0) return prev;
-	if( num == 1) return curr;
+    if( num == 0 && num == 1) return prev;
+	if( num == 2) return curr;
 	
     for( let i = 2; i <= num; i++){
 	    let next = prev + curr;   //next==1
